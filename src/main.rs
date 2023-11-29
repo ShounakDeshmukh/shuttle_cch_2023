2c3a1541d86c2ed2ae7ceae6bde6d132da3c963d
ShounakDeshmukh <shounsach@gmail.com>
2023-11-29 20:27:11 +0530

    challenge -1 done also initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router, http::StatusCode};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn fake_error()-> StatusCode{
+    StatusCode::INTERNAL_SERVER_ERROR
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+    .route("/", get(hello_world))
+    .route("/-1/error", get(fake_error));
+    Ok(router.into())
+}
